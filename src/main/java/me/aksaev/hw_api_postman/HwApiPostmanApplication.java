f01666578dc66a44de8a7e23cf76e0abed87287d
Aksaev <mail-ax@ya.ru>
2022-12-26 20:50:36 +0400

    Initial commit

---
+++ src/main/java/me/aksaev/hw_api_postman/HwApiPostmanApplication.java
@@ -0,0 +1,13 @@
+package me.aksaev.hw_api_postman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwApiPostmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwApiPostmanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/aksaev/hw_api_postman/HwApiPostmanApplicationTests.java
@@ -0,0 +1,13 @@
+package me.aksaev.hw_api_postman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwApiPostmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
